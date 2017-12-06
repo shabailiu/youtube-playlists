2f0086636ea77922894564bf152e5115b48b64dc
Shabai Liu <shliu@paypal.com>
2017-12-06 16:52:56 -0500

    Unsubscribe

--- app/actions/SubscriptionActions.js
+++ app/actions/SubscriptionActions.js
@@ -1,11 +1,19 @@
 import * as action from '../constants/SubscriptionConstants';
 
-export const subscribeToPlaylist = playlistId => ({
-  type: action.SUBSCRIBE_TO_PLAYLIST,
-  payload: playlistId
-});
+export const subscribeToPlaylist = playlistId => {
+  return dispatch => {
+    dispatch({
+      type: action.SUBSCRIBE_TO_PLAYLIST,
+      payload: playlistId
+    });
+  };
+};
 
 export const unsubscribeFromPlaylist = playlistId => ({
   type: action.UNSUBSCRIBE_FROM_PLAYLIST,
   payload: playlistId
 });
+
+const readFromPlaylistFeed = playlistId => {
+
+};
--- app/reducers/playlists.js
+++ app/reducers/playlists.js
@@ -2,9 +2,11 @@ import * as PlaylistAction from '../constants/PropTypeValidation';
 import * as SubscriptionAction from '../constants/SubscriptionConstants';
 
 export default (state = {}, action) => {
+  let playlistId;
+
   switch (action.type) {
     case SubscriptionAction.SUBSCRIBE_TO_PLAYLIST:
-      const playlistId = action.payload;
+      playlistId = action.payload;
 
       if (state[playlistId]) {
         return state;
@@ -16,6 +18,17 @@ export default (state = {}, action) => {
           feedUrl: `https://www.youtube.com/feeds/videos.xml?playlist_id=${playlistId}`
         }
       };
+    case SubscriptionAction.UNSUBSCRIBE_FROM_PLAYLIST:
+      playlistId = action.payload;
+
+      if (!state[playlistId]) {
+        return state;
+      }
+
+      const newState = {...state};
+      delete newState[playlistId];
+
+      return newState;
     default:
       return state;
   }
