33cd2a712ee00fc9f26b9627364f2c15bb0198f6
Shabai Liu <shliu@paypal.com>
2017-12-07 17:57:16 -0500

    Filter videos

--- app/components/VideoDisplay/VideoDisplay.js
+++ app/components/VideoDisplay/VideoDisplay.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
 import VideoGridCard from '../VideoGridCard/VideoGridCard';
 import { videoShape } from '../../constants/PropTypeValidation';
+import { filterVideos, FILTER_BY } from './VideoDisplayUtils';
 
 export class VideoDisplay extends Component {
   static propTypes = {
@@ -10,13 +12,14 @@ export class VideoDisplay extends Component {
 
   render() {
     const { videos } = this.props;
+    const filteredVideos = filterVideos(videos, 12, FILTER_BY.LAST_YEAR);
     const videoArr = [];
 
-    if (!videos) {
+    if (isEmpty(filteredVideos)) {
       return <div>No videos</div>;
     }
 
-    videos.forEach(video => {
+    filteredVideos.forEach(video => {
       videoArr.push(
         <li
           key={video.videoId}
---
+++ app/components/VideoDisplay/VideoDisplayUtils.js
@@ -0,0 +1,34 @@
+import moment from 'moment';
+
+export const FILTER_BY = {
+  DEFAULT: 'DEFAULT',
+  TODAY: 'TODAY',
+  LAST_3_DAYS: 'LAST_3_DAYS',
+  LAST_WEEK: 'LAST_WEEK',
+  LAST_MONTH: 'LAST_MONTH',
+  LAST_YEAR: 'LAST_YEAR'
+};
+
+const mapFilterToMoment = {
+  [FILTER_BY.DEFAULT]: moment().subtract(3, 'days'),
+  [FILTER_BY.TODAY]: moment().subtract(1, 'day'),
+  [FILTER_BY.LAST_3_DAYS]: moment().subtract(3, 'days'),
+  [FILTER_BY.LAST_WEEK]: moment().subtract(1, 'week'),
+  [FILTER_BY.LAST_MONTH]: moment().subtract(1, 'month'),
+  [FILTER_BY.LAST_YEAR]: moment().subtract(1, 'year')
+};
+
+/**
+ * Filter which videos to show, and sort by descending uploaded timestamp.
+ * @param videos
+ * @param maxResults
+ * @param filterMethod FILTER_BY
+ */
+//TODO this may be optimized if the RSS feed is already guaranteed to be sorted
+export const filterVideos = (videos, maxResults = 12, filterMethod = FILTER_BY.DEFAULT) => {
+  const minTime = mapFilterToMoment[filterMethod];
+  return videos
+    .filter(video => moment(video.uploadedTimestamp).isAfter(minTime))
+    .sort((a, b) => new Date(b.uploadedTimestamp) - new Date(a.uploadedTimestamp))
+    .slice(0, maxResults);
+};
